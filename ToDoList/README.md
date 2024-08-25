60c36c98b33b040c3ade73fe87f1c42397ce1579
Ayush Singh <2ayushsingh@gmail.com>
2024-08-25 10:17:08 +0530

    Added New Functions

--- ToDoList/README.md
+++ ToDoList/README.md
@@ -1,2 +1,12 @@
 # ToDo List
-Hello everyone, this is a basic todo list completely made by me as my first project on github
+Hello everyone, this is a basic todo list completely made by me as my first project on github.<br>
+<h2>Functionalities of this ToDo List Application</h2>
+
+1. Display your tasks list.<br>
+2. Update task status.<br>
+3. Delete a single task.<br>
+4. Delete all tasks.<br>
+5. Save and exit.<br>
+6. Exit without saving.<br>
+
+<h1>Thankyou for visiting.<h1>
--- ToDoList/ToDoList.py
+++ ToDoList/ToDoList.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 class ToDo:
     def __init__(self, file_path):
@@ -9,42 +10,48 @@ class ToDo:
 
     def load_list(self):
         """Load JSON file"""
-        try:
-            with open(self.file_path, 'r') as file:
-                self.task_status = json.load(file)
-                self.task_list = list(self.task_status.keys())
-        except FileNotFoundError:
-            print("The ToDo list does not exist")
+        if not os.path.exists("E:\\Programming\\Python\\JSON\\ToDo.json"):
+            with open("E:\\Programming\\Python\\JSON\\ToDo.json", 'w') as f:
+                f.write('{}')
+        with open(self.file_path, 'r') as file:
+            self.task_status = json.load(file)
+
+    def empty_list(self):
+        print("The task list is empty.\nPlease enter some tasks first.")
 
     def add_task(self, task):
         """Add task to list"""
         if task in list(self.task_status.keys()):
             print(f"The task {task} already exists.")
+        elif task =="":
+            print("Empty task. Enter a task.")
         else:
             self.task_status.setdefault(task, "pending")
         
-
     def add_status(self):
         """Task is completed or pending"""
-        print("Default status of tasks is pending.")
-        print("Enter 1 for completed and 0 for pending")
-        print("Task (current status) : (new status)")
-        for todos in self.task_status.keys():
-            while True:
-                print(f"{todos} ({self.task_status[todos]}) : ", end="")
-                try:
-                    status = int(input())
-                    if status == 1:
-                        self.task_status[todos] = "completed"
-                        break
-                    elif status == 0:
-                        self.task_status[todos] = "pending"
-                        break
-                    else:
-                        print("Invalid input. Status remains unchanged.")
-                except ValueError:
-                    print("Please enter either 0 or 1.")
-        print("Status Updated Successfully.")
+        if self.task_status:
+            print("Default status of tasks is pending.")
+            print("Enter 1 for completed and 0 for pending")
+            print("Task (current status) : (new status)")
+            for todos in self.task_status.keys():
+                while True:
+                    print(f"{todos} ({self.task_status[todos]}) : ", end="")
+                    try:
+                        status = int(input())
+                        if status == 1:
+                            self.task_status[todos] = "completed"
+                            break
+                        elif status == 0:
+                            self.task_status[todos] = "pending"
+                            break
+                        else:
+                            print("Invalid input. Status remains unchanged.")
+                    except ValueError:
+                        print("Please enter either 0 or 1.")
+            print("Status Updated Successfully.")
+        else:
+            self.empty_list()
 
     def save_tasks(self):
         """Saves tasks into json file"""
@@ -61,10 +68,39 @@ class ToDo:
                 print(f"{i}. {task} --> {status}")
                 i = i + 1
         else:
-            print("No tasks are added.")
+            self.empty_list()
+
     def del_todo(self):
-        self.task_status.clear()
-        print("Deleted all tasks successfully")
+        if self.task_status:
+            print("Do you really want to delete all tasks: ")
+            while True:
+                confirm = input("Enter 'y' for yes and 'n' for no: ")
+                if confirm == 'y':
+                    self.task_status.clear()
+                    print("Deleted all tasks successfully.")
+                    break
+                elif confirm == 'n':
+                    print("Tasks not deleted.")
+                    break
+                else:
+                    print("Invalid input.")
+        else:
+            self.empty_list()
+
+    def del_task(self):
+        if self.task_status:
+            for i, task in enumerate(list(self.task_status.keys()), 1):
+                print(f"{i}. {task}")
+            while True:
+                del_task = input("Enter name of the task to delete: ").strip().lower() 
+                if del_task in list(list(self.task_status)):
+                    del self.task_status[del_task]
+                    print("Task Deleted Successfully.")
+                    break
+                else:
+                    print("There is no such task\nPlease enter a valid task name.")
+        else:
+            self.empty_list()
 
     def work(self):
         """main function"""
@@ -72,10 +108,11 @@ class ToDo:
             print("""
             Enter 0 to add a new task
             Enter 1 to show ToDo list
-            Enter 2 to save and exit
-            Enter 3 to add or update status
-            Enter 4 to exit without saving
-            Enter 5 to delete all tasks
+            Enter 2 to add or update status
+            Enter 3 to delete a single task
+            Enter 4 to delete all tasks
+            Enter 5 to save and exit
+            Enter 6 to exit without saving
             """)
             choice = int(input())
             match (choice):
@@ -89,15 +126,17 @@ class ToDo:
                 case 1:
                     self.show_todo()
                 case 2:
+                    self.add_status()
+                case 3:
+                    self.del_task()
+                case 4:
+                    self.del_todo()
+                case 5:
                     self.save_tasks()
                     break
-                case 3:
-                    self.add_status()
-                case 4:
+                case 6:
                     print("Exiting without saving")
                     break
-                case 5:
-                    self.del_todo()
                 case _:
                     print("Invalid Input") 
 
